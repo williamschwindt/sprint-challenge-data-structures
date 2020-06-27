3e1dd6274f40281114f3c853ba38ba2a053ecffb
William Schwindt <williammschwindt@gmail.com>
2020-06-26 17:11:36 -0700

    ring buffer complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        self.buffer = [None for el in range(0, self.capacity)]
+        self.curr = 0
 
     def append(self, item):
-        pass
+        #add item at the index of self.curr and increment curr
+        self.buffer[self.curr] = item
+        self.curr += 1
+        
+        #reset curr if its at capacity
+        if self.curr == self.capacity:
+            self.curr = 0
 
     def get(self):
-        pass
+        returned_arr = []
+        for el in self.buffer:
+            if el is not None:
+                returned_arr.append(el)
+        return returned_arr
